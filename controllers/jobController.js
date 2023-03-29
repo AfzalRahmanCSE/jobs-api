f8ca1a109b73bc65ab313048daf1f8a4b0c9e634
AfzalUrRahman <afzalrahman.cse@gmail.com>
2023-03-29 23:20:52 +0530

    Get Jobs Code Added

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,7 +1,29 @@
 const Jobs=require('./../models/jobs')
+const User=require('./../models/users')
 
 exports.getJobs=async (req,res,next)=>{
-    res.status(200).send({message:'Page Coming Soon'})
+        try{
+        const resPerPage=2;
+        const currentPage=+req.query.page||1;
+        const skip=resPerPage*(currentPage-1)
+        
+        const keyword=req.query.keyword
+            ?{
+               title:{
+                $regex:req.query.keyword,
+                $options:'i'
+               }     
+            }:{}
+
+        const jobs=await Jobs.find({...keyword})
+                        .populate('user','name email -_id').select('-__v')
+                        .limit(resPerPage).skip(skip)
+
+        res.status(200).send(jobs)
+    }catch(error){
+        res.status(500)
+        res.send("Internal Server error")
+    }
 }
 
 exports.newJob=async (req,res,next)=>{
